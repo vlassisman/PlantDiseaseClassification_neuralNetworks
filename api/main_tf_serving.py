04f9e0c47b9fa1e08a461dda95ddc85c68c26567
Vlasis <vlaskou@hotmail.com>
2022-08-18 01:33:56 +0300

    tutorial 6

--- api/main_tf_serving.py
+++ api/main_tf_serving.py
@@ -56,4 +56,4 @@ async def predict(
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
+    uvicorn.run(app, host='127.0.0.1', port=8000)
--- training/training_ImageDataGenerator.py
+++ training/training_ImageDataGenerator.py
@@ -7,6 +7,7 @@ from keras.preprocessing.image import ImageDataGenerator
 IMAGE_SIZE = 256
 CHANNELS = 3
 
+# TRAINING
 train_datagen = ImageDataGenerator(
     rescale=1./255,
     horizontal_flip=True,
@@ -14,10 +15,89 @@ train_datagen = ImageDataGenerator(
 )
 
 train_generator = train_datagen.flow_from_directory(
-    '/dataset/train',
+    'C:/dev/git/PlantDiseaseClassification_neuralNetworks/training/dataset/train',
     target_size=(IMAGE_SIZE, IMAGE_SIZE),
     batch_size=32,
-    class_mode='sparse',
-    save_to_dir="AugmentedImages"
+    class_mode='sparse'
 )
 
+# VALIDATION
+validation_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=10,
+    horizontal_flip=True
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+    'C:/dev/git/PlantDiseaseClassification_neuralNetworks/training/dataset/val',
+    target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=32,
+    class_mode="sparse"
+)
+
+# TEST
+test_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=10,
+    horizontal_flip=True
+)
+
+test_generator = test_datagen.flow_from_directory(
+    'C:/dev/git/PlantDiseaseClassification_neuralNetworks/training/dataset/test',
+    target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=32,
+    class_mode="sparse"
+)
+
+# MODEL
+input_shape = (IMAGE_SIZE, IMAGE_SIZE, CHANNELS)
+n_classes = 3
+
+model = models.Sequential([
+    layers.InputLayer(input_shape=input_shape),
+
+    # convolution - pooling stack 
+    layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=input_shape),
+    layers.MaxPooling2D((2, 2)),
+
+    layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    
+    layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+
+    layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+
+    layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+
+    layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+
+    # flatten data - dense layer - output
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(n_classes, activation='softmax')  ## normalization
+])
+
+model.summary()
+
+model.compile(
+    optimizer='adam',  ## sthg like gradient descent
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+    metrics=['accuracy']
+)
+
+# NETWORK TRAINING
+history = model.fit(
+    train_generator,
+    steps_per_epoch=47,
+    batch_size=32,
+    verbose=1, # prints whats going on
+    validation_data=validation_generator,
+    validation_steps=6,
+    epochs=20
+)
+
+model.save('../_potatoes.h5')
